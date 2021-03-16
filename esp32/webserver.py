39bb2c46de2c4c85d334808b35da52c65c23c716
David Pérez <davidpm.itengineer@gmail.com>
2021-03-16 20:34:26 +0100

    added: ESP32: webserver example

---
+++ esp32/webserver.py
@@ -0,0 +1,98 @@
+import network
+import utime
+import socket
+
+from dht import DHT11
+from machine import Pin
+
+
+CONFIG_AP = False
+SERVER_PORT = 8080
+SENSOR_DHT_PIN = 23
+
+def startWifiClient():
+
+    wifi = network.WLAN(network.STA_IF)
+    wifi.active(True)
+    wifi.connect("Nenukines-Haus", "8V83QGJR773E9767")
+    while not wifi.isconnected():
+        utime.sleep(1)
+    
+    wifi.ifconfig(["192.168.1.210", "255.255.255.0", "192.168.1.1", "192.168.1.1"])
+    utime.sleep(1)
+        
+        
+def startWifiAp():
+
+    wifi = network.WLAN(network.AP_IF)
+    wifi.active(True)
+    wifi.config(essid='ESP32-TEST-AP', authmode=network.AUTH_OPEN)
+    
+    
+def startServer(dispatchFunc, dispatchObj, port):
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('', port))
+    s.listen(5)
+    
+    print("Listening on port {0}...".format(port))
+    
+    while True:
+        conn, addr = s.accept()
+        try:
+            print("Got a connection from {0}".format(addr))
+            request = conn.recv(1024)
+            print(str(request))
+        
+            response = dispatchFunc(dispatchObj)
+            
+            conn.send('HTTP/1.1 200 OK\n')
+            conn.send('Content-Type: text/html\n')
+            conn.send('Connection: close\n\n')
+            conn.sendall(response)
+            conn.close()
+            
+        except ex:
+            print(ex)
+
+
+def dispatch(obj):
+
+    sensor = obj["sensor"]
+    sensor.measure()
+    temp = sensor.temperature()
+    humi = sensor.humidity()
+    time = utime.time_ns()
+    
+
+    html = """
+            <html>
+                <head>
+                    <title>ESP32 Example</title>
+                </head>
+                <body>
+                    <table>
+                        <tr><td><b>Temperature</b></td><td>{0} C</td></tr>
+                        <tr><td><b>Humidity</b></td><td>{1} %</td></tr>
+                        <tr><td><b>Time</b></td><td>{2}</td></tr>
+                    </table>
+                </body>
+            </html>
+        """.format(temp,humi, time)
+
+    return html
+
+
+def main():
+
+    if CONFIG_AP:
+        startWifiAp()
+    else:
+        startWifiClient()
+        
+    sensor = DHT11(Pin(SENSOR_DHT_PIN))
+    startServer(dispatch, { "sensor": sensor }, SERVER_PORT)
+    
+    
+if __name__ == '__main__':
+    main()
